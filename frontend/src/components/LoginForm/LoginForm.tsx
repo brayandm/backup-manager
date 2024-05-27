cd2f1fdc13c86684428b7b86ad20de7281555044
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-05-27 18:51:29 +0200

    Add LoginForm component

---
+++ frontend/src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import {
+  Box,
+  TextField,
+  Button,
+  Alert,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import React, { FormEvent, useState } from "react";
+import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { useRouter } from "next/navigation";
+
+const LoginForm: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [timerId, setTimerId] = useState<NodeJS.Timeout | undefined>(undefined);
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const res = await signIn("credentials", {
+      email: email,
+      password: password,
+      redirect: false,
+    });
+
+    if (res) {
+      if (res?.error) {
+        clearTimeout(timerId);
+        setError(true);
+        setTimerId(setTimeout(() => setError(false), 2000));
+        return;
+      }
+    }
+
+    const callbackUrl = searchParams.get("callbackUrl") || "/dashboard";
+
+    router.push(callbackUrl);
+  };
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        maxWidth: "300px",
+        margin: "0 auto",
+      }}
+      onSubmit={handleSubmit}
+    >
+      <Box
+        component="div"
+        sx={{
+          height: "50px",
+          margin: "10px 0",
+        }}
+      >
+        {error && <Alert severity="error">Bad credentials</Alert>}
+      </Box>
+      <TextField
+        id="email"
+        label="Email"
+        variant="outlined"
+        margin="normal"
+        type="email"
+        required
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <FormControl
+        variant="outlined"
+        sx={{ marginBottom: "20px", height: "56px" }}
+      >
+        <InputLabel htmlFor="outlined-adornment-password" required>
+          Password
+        </InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type={showPassword ? "text" : "password"}
+          value={password}
+          required
+          onChange={(event) => setPassword(event.target.value)}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </FormControl>
+      <Button variant="contained" type="submit" sx={{ marginTop: "16px" }}>
+        Sign In
+      </Button>
+    </Box>
+  );
+};
+
+export default LoginForm;
