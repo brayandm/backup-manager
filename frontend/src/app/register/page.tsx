045e2d0ac944926896b3d277aff87bfc7f9faa18
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-05-28 00:38:41 +0200

    Update redirect path in register page

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -7,7 +7,7 @@ export default async function Register() {
   const session = await getServerSession(authOptions);
 
   if (session) redirect("/dashboard");
-  if (!(await isFirstUse())) redirect("/register");
+  if (!(await isFirstUse())) redirect("/login");
 
   return <RegisterForm />;
 }
