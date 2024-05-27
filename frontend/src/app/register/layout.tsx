1e80aeb7342b7cefbbf6573413005f79f4fc972d
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-05-27 23:13:53 +0200

    Add register page and update middleware configuration

---
+++ frontend/src/app/register/layout.tsx
@@ -0,0 +1,7 @@
+export default function RegisterLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,13 +1,13 @@
-import LoginForm from "@/components/LoginForm";
+import RegisterForm from "@/components/RegisterForm";
 import { getServerSession } from "next-auth";
 import { authOptions, isFirstUse } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
-export default async function Login() {
+export default async function Register() {
   const session = await getServerSession(authOptions);
 
   if (session) redirect("/dashboard");
-  if (await isFirstUse()) redirect("/register");
+  if (!(await isFirstUse())) redirect("/register");
 
-  return <LoginForm />;
+  return <RegisterForm />;
 }
