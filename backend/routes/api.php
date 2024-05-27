e790452f2a01c029d25836857c270c55652707f3
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-05-27 16:31:04 +0200

    Refactor API routes and add new endpoints

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function () {
+
+    Route::post('/login', 'App\Http\Controllers\AuthController@login');
+
+    // Route::post('/register', 'App\Http\Controllers\AuthController@register');
+
+    Route::middleware(['check.constant.connection', 'auth:sanctum'])->group(function () {
+
+        Route::get('/verify', 'App\Http\Controllers\AuthController@verify');
+
+        Route::post('/logout', 'App\Http\Controllers\AuthController@logout');
+
+        Route::post('/logoutall', 'App\Http\Controllers\AuthController@logoutall');
+
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+    });
 });
