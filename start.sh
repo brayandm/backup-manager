cdeadd72a71ed8543f8f39834fe9add2756b0da6
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-05-28 06:45:33 +0200

    Fix typo in docker-compose.yml URL

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -o docker-compose.yml https://raw.githubusercontent.com/brayandm/backup-manager/main/docker-compose.ym
-docker-compose up -d
+curl -o docker-compose.yml https://raw.githubusercontent.com/brayandm/backup-manager/main/docker-compose.yml
+docker compose up -d
