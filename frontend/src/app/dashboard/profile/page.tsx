73e1648d4dffbf3c606c2ee995cce5325c71bf22
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-05-29 02:38:25 +0200

    Add serverGet function to fetch user data

--- frontend/src/app/dashboard/profile/page.tsx
+++ frontend/src/app/dashboard/profile/page.tsx
@@ -1,10 +1,13 @@
 import Logout from "@/components/Logout";
 import UserProfile from "@/components/UserProfile";
 import { isSessionValid } from "@/lib/auth";
+import { serverGet } from "@/lib/backendApi";
 
 export default async function Profile() {
+  const res = await serverGet("/user");
+
   if (!(await isSessionValid())) {
     return <Logout />;
   }
-  return <UserProfile />;
+  return <UserProfile user={res.data} />;
 }
