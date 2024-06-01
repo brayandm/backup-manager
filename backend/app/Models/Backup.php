17c165b1b0cce7446e22f16112d44d798a16c210
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-01 21:30:09 +0200

    Add connection_config cast to Backup and StorageServer models

--- backend/app/Models/Backup.php
+++ backend/app/Models/Backup.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Casts\ConnectionCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,10 @@ class Backup extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    protected $casts = [
+        'connection_config' => ConnectionCast::class,
+    ];
 
     public function storageServer()
     {
