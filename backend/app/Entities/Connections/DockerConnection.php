4bf87a497c44ed790c341c96962eb3a689311d0c
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-01 20:49:45 +0200

    Add constructor to DockerConnection and SshConnection classes

--- backend/app/Entities/Connections/DockerConnection.php
+++ backend/app/Entities/Connections/DockerConnection.php
@@ -6,6 +6,14 @@ use App\Interfaces\ConnectionInterface;
 
 class DockerConnection implements ConnectionInterface
 {
+    private $container_name;
+
+    public function __construct(string $container_name)
+    {
+        $this->container_name = $container_name;
+
+    }
+
     public function Run()
     {
     }
--- backend/app/Entities/Connections/SshConnection.php
+++ backend/app/Entities/Connections/SshConnection.php
@@ -6,6 +6,20 @@ use App\Interfaces\ConnectionInterface;
 
 class SshConnection implements ConnectionInterface
 {
+    private $user;
+    private $host;
+    private $port;
+    private $private_key;
+    private $passphrase;
+
+    public function __construct(string $user, string $host, string $port, string $private_key, ?string $passphrase)
+    {
+        $this->user = $user;
+        $this->host = $host;
+        $this->port = $port;
+        $this->private_key = $private_key;
+        $this->passphrase = $passphrase;
+    }
     public function Run()
     {
 
