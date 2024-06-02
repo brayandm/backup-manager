0869e4dbc4f0aa515ee236b7d4cdf43502ee6353
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-02 05:45:33 +0200

    Update casts in Backup and StorageServer models

--- backend/app/Models/Backup.php
+++ backend/app/Models/Backup.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Casts\ConnectionCast;
+use App\Casts\DriverCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,7 +15,7 @@ class Backup extends Model
 
     protected $casts = [
         'connection_config' => ConnectionCast::class,
-        'driver_config' => ConnectionCast::class,
+        'driver_config' => DriverCast::class,
     ];
 
     public function storageServer()
