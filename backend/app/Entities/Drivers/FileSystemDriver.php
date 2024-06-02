ce812ccf7bc212bc5907664f45714da6450f462a
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-02 04:03:53 +0200

    Refactor file system driver commands***

--- backend/app/Entities/Drivers/FileSystemDriver.php
+++ backend/app/Entities/Drivers/FileSystemDriver.php
@@ -15,20 +15,24 @@ class FileSystemDriver implements DriverInterface
 
     public function Push(string $filepath)
     {
-        $command = "cp $filepath $this->path";
+        $command = "tar -xzf $filepath $this->path";
 
         return $command;
     }
 
     public function Pull(string $filepath)
     {
-        $command = "cp $this->path $filepath";
+        $command = "tar -czf $filepath $this->path";
 
         return $command;
+
     }
 
     public function Setup()
     {
+        $command = "command -v tar >/dev/null 2>&1 || { sudo apt-get update -qq && sudo apt-get install -y -qq tar; }";
+
+        return $command;
     }
 
     public function Clean()
