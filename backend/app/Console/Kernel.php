1d39677cbf7ecd724ac4437a5aed6f85149e9004
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-03 03:40:33 +0200

    Fix capitalization of BackupConfiguration class

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -2,7 +2,7 @@
 
 namespace App\Console;
 
-use App\Models\backupConfiguration;
+use App\Models\BackupConfiguration;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $backupConfigurations = backupConfiguration::all();
+        $backupConfigurations = BackupConfiguration::all();
 
         foreach ($backupConfigurations as $backupConfiguration) {
             $schedule->command('app:run-backup-configuration', ['id' => $backupConfiguration->id])
