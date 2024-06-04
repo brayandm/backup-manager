89a98005ffac94641c32f07c0dfcd9d3cdc081cb
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-04 11:32:55 +0200

    Add LayerInterface

---
+++ backend/app/Interfaces/LayerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Interfaces;
+
+interface LayerInterface
+{
+    public function Apply(string $localWorkDir);
+
+    public function Unapply(string $localWorkDir);
+
+    public function Setup();
+
+    public function Clean();
+}
