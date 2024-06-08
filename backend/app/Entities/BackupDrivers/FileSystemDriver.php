48bf32629e0e82b34548d07cf70b0b4d50c30093
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-08 22:24:46 +0200

    Fix command concatenation in FileSystemDriver.php

--- backend/app/Entities/BackupDrivers/FileSystemDriver.php
+++ backend/app/Entities/BackupDrivers/FileSystemDriver.php
@@ -21,7 +21,7 @@ class FileSystemDriver implements BackupDriverInterface
     {
         $command = "rm -r -f $this->contextPath";
 
-        $command .= " && ".$compressionMethod->decompress("$localWorkDir/*", $this->contextPath);
+        $command .= ' && '.$compressionMethod->decompress("$localWorkDir/*", $this->contextPath);
 
         $command .= " && rm -r -f $localWorkDir";
 
@@ -32,7 +32,7 @@ class FileSystemDriver implements BackupDriverInterface
     {
         $command = "mkdir $localWorkDir -p";
 
-        $command .= " && ".$compressionMethod->compress($this->contextPath, $localWorkDir);
+        $command .= ' && '.$compressionMethod->compress($this->contextPath, $localWorkDir);
 
         return $command;
     }
