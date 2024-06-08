022f95866769ef2c0a2d407f0644978afd30e2df
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-08 21:57:49 +0200

    Add CompressionMethodCast to Backup model

--- backend/app/Models/Backup.php
+++ backend/app/Models/Backup.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Casts\CompressionMethodCast;
 use App\Casts\ConnectionCast;
 use App\Casts\StorageServerDriverCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -16,6 +17,7 @@ class Backup extends Model
     protected $casts = [
         'connection_config' => ConnectionCast::class,
         'driver_config' => StorageServerDriverCast::class,
+        'compression_config' => CompressionMethodCast::class,
     ];
 
     public function backupConfiguration()
