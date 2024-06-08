038de46c242fcdbadca89c2d9b4ce6e33fa2f6ee
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-08 15:08:32 +0200

    Remove unused import statements

--- backend/app/Models/BackupConfiguration.php
+++ backend/app/Models/BackupConfiguration.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use App\Casts\BackupDriverCast;
 use App\Casts\ConnectionCast;
-use App\Services\BackupService;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
