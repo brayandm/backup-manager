a70415e3974e97f69df83c2bd262ba384bcfce0e
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-09 19:38:38 +0200

    Refactor integrity check method to include hash verification

--- backend/app/Entities/Methods/IntegrityCheckMethods/Sha256SumIntegrityCheckMethod.php
+++ backend/app/Entities/Methods/IntegrityCheckMethods/Sha256SumIntegrityCheckMethod.php
@@ -15,7 +15,7 @@ class Sha256SumIntegrityCheckMethod implements IntegrityCheckMethodInterface
 
     public function verify(string $localWorkDir)
     {
-        $command = 'true';
+        $command = "cd $localWorkDir && find . -type f -exec sha256sum {} + | sha256sum | awk '{print \$1}' | grep -xq '{$this->hash}' || { echo 'Hash mismatch. Integrity check failed.' >&2; exit 1; }";
 
         return $command;
     }
