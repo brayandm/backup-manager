d7e90a3201f7f3eedfc09cf1fc23d45567e2627c
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-09 02:04:49 +0200

    Refactor encryption method commands

--- backend/app/Entities/Methods/EncryptionMethods/Aes256CbcEncryptionMethod.php
+++ backend/app/Entities/Methods/EncryptionMethods/Aes256CbcEncryptionMethod.php
@@ -16,12 +16,14 @@ class Aes256CbcEncryptionMethod implements EncryptionMethodInterface
     public function encrypt(string $localWorkDir)
     {
         $command = "find \"$localWorkDir\" -type f -name '*' -exec sh -c 'openssl enc -aes-256-cbc -salt -pbkdf2 -in \"\$1\" -out \"\$1.enc\" -pass pass:\"$this->key\" && rm \"\$1\"' _ {} \\;";
+
         return $command;
     }
 
     public function decrypt(string $localWorkDir)
     {
         $command = "find \"$localWorkDir\" -type f -name '*.enc' -exec sh -c 'openssl enc -d -aes-256-cbc -pbkdf2 -in \"\$1\" -out \"\${1%.enc}\" -pass pass:\"$this->key\" && rm \"\$1\"' _ {} \\;";
+
         return $command;
     }
 
