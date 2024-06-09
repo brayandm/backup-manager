fe4aeb71e578bc7b68906ca0b6e4c3137d992369
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-09 03:15:38 +0200

    Refactor encryption and decryption methods

--- backend/app/Entities/Methods/EncryptionMethods/Aes256CbcEncryptionMethod.php
+++ backend/app/Entities/Methods/EncryptionMethods/Aes256CbcEncryptionMethod.php
@@ -19,7 +19,7 @@ class Aes256CbcEncryptionMethod implements EncryptionMethodInterface
         $tempDir = $localWorkDir.'/'.Str::uuid();
         $encrypt = "openssl enc -aes-256-cbc -salt -pbkdf2 -in \"\$1\" -out \"$tempDir/tmp.enc\" -pass pass:\"$this->key\" && mv \"$tempDir/tmp.enc\" \"\$1\"";
 
-        $command = "find \"$localWorkDir\" -type d | sed 's|^$localWorkDir||' | awk -v tempDir=\"$tempDir\" '{print \"mkdir -p \\\"\" tempDir \$0 \"\\\"\"}' | sh";
+        $command = "mkdir -p \"$tempDir\"";
         $command .= " && find \"$localWorkDir\" -type f -name '*' -exec sh -c '$encrypt' _ {} \\;";
         $command .= " && rm -rf \"$tempDir\"";
 
@@ -31,7 +31,7 @@ class Aes256CbcEncryptionMethod implements EncryptionMethodInterface
         $tempDir = $localWorkDir.'/'.Str::uuid();
         $decrypt = "openssl enc -d -aes-256-cbc -pbkdf2 -in \"\$1\" -out \"$tempDir/tmp.dec\" -pass pass:\"$this->key\"&& mv \"$tempDir/tmp.dec\" \"\$1\"";
 
-        $command = "find \"$localWorkDir\" -type d | sed 's|^$localWorkDir||' | awk -v tempDir=\"$tempDir\" '{print \"mkdir -p \\\"\" tempDir \$0 \"\\\"\"}' | sh";
+        $command = "mkdir -p \"$tempDir\"";
         $command .= " && find \"$localWorkDir\" -type f -name '*' -exec sh -c '$decrypt' _ {} \\;";
         $command .= " && rm -rf \"$tempDir\"";
 
