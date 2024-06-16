55d04e0d0d21860d4736e0571eb944d8235bfbe3
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-16 17:57:42 +0200

    Refactor BackupFactory to use new BackupConfiguration model and update backup creation logic

--- backend/database/factories/BackupFactory.php
+++ backend/database/factories/BackupFactory.php
@@ -2,7 +2,35 @@
 
 namespace Database\Factories;
 
+use App\Entities\BackupDriverConfig;
+use App\Entities\BackupDrivers\FileSystemDriver;
+use App\Entities\CompressionMethodConfig;
+use App\Entities\ConnectionConfig;
+use App\Entities\Connections\SshConnection;
+use App\Entities\EncryptionMethodConfig;
+use App\Entities\IntegrityCheckMethodConfig;
+use App\Entities\Methods\CompressionMethods\TarCompressionMethod;
+use App\Entities\Methods\EncryptionMethods\Aes256CbcEncryptionMethod;
+use App\Entities\Methods\IntegrityCheckMethods\Sha256SumIntegrityCheckMethod;
+use App\Models\BackupConfiguration;
 use Illuminate\Database\Eloquent\Factories\Factory;
+
+/*
+Schema::create('backups', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('backup_configuration_id')->constrained();
+            $table->string('name');
+            $table->json('connection_config');
+            $table->json('driver_config');
+            $table->json('compression_config');
+            $table->json('encryption_config');
+            $table->json('integrity_check_config');
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+*/
+
+// $backup->name = 'backup-'.$this->formatText($backupConfiguration->name).'-'.$this->formatText($storageServer->name).'-'.'id'.$backup->id.'-'.date('Ymd-His').'-UTC';
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Backup>
@@ -17,7 +45,33 @@ class BackupFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'backup_configuration_id' => BackupConfiguration::factory(),
+            'name' => 'backup-backup_configuration_name-storage_server_name-id'.rand(1, 1000).'-'.date('Ymd-His').'-UTC',
+            'connection_config' => new ConnectionConfig([
+                new SshConnection(
+                    'brayand',
+                    'localhost',
+                    '22',
+                    'path',
+                    '/home/brayand/.ssh/local',
+                    null
+                )]),
+            'driver_config' => new BackupDriverConfig(
+                new FileSystemDriver(
+                    '/home/brayand/Storage/Personal/Capstone/Testing/Proyecto/DataImportante/'
+                )
+            ),
+            'compression_config' => new CompressionMethodConfig(
+                new TarCompressionMethod()
+            ),
+            'encryption_config' => new EncryptionMethodConfig(
+                new Aes256CbcEncryptionMethod(null)
+            ),
+            'integrity_check_config' => new IntegrityCheckMethodConfig
+            (
+                new Sha256SumIntegrityCheckMethod(null)
+            ),
+            'status' => rand(2, 3),
         ];
     }
 }
