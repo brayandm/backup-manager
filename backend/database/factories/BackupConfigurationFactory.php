1a6088d0a0d34144765e9204e7f57ae152eb3833
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-16 15:11:12 +0200

    feat: Add BackupConfigurationFactory with default values for backup configuration

--- backend/database/factories/BackupConfigurationFactory.php
+++ backend/database/factories/BackupConfigurationFactory.php
@@ -2,6 +2,16 @@
 
 namespace Database\Factories;
 
+use App\Entities\BackupDriverConfig;
+use App\Entities\BackupDrivers\FileSystemDriver;
+use App\Entities\CompressionMethodConfig;
+use App\Entities\ConnectionConfig;
+use App\Entities\Connections\SshConnection;
+use App\Entities\EncryptionMethodConfig;
+use App\Entities\IntegrityCheckMethodConfig;
+use App\Entities\Methods\CompressionMethods\TarCompressionMethod;
+use App\Entities\Methods\EncryptionMethods\Aes256CbcEncryptionMethod;
+use App\Entities\Methods\IntegrityCheckMethods\Sha256SumIntegrityCheckMethod;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +27,32 @@ class BackupConfigurationFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name,
+            'connection_config' => new ConnectionConfig([
+                new SshConnection(
+                    "brayand",
+                    "localhost",
+                    "22",
+                    "path",
+                    "/home/brayand/.ssh/local",
+                    null
+                )]),
+            'driver_config' => new BackupDriverConfig(
+                new FileSystemDriver(
+                    "/home/brayand/Storage/Personal/Capstone/Testing/Proyecto/DataImportante/"
+                )
+            ),
+            'schedule_cron' => '0 0 * * *',
+            'retention_policy_config' => "{}",
+            'compression_config' => new CompressionMethodConfig(
+                new TarCompressionMethod()
+            ),
+            'encryption_config' => new EncryptionMethodConfig(
+                new Aes256CbcEncryptionMethod(null)
+            ),
+            'integrity_check_config' => new IntegrityCheckMethodConfig(
+                new Sha256SumIntegrityCheckMethod(null)
+            ),
         ];
     }
 }
