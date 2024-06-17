3a5721fc9285cdbcf98b8737111ab6ce1810bc4c
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-17 23:30:47 +0200

    refactor: Update EnhancedTable component to accept headCells and rows as props

--- frontend/src/components/Dashboard/BackupConfigurations/BackupConfigurations.tsx
+++ frontend/src/components/Dashboard/BackupConfigurations/BackupConfigurations.tsx
@@ -7,11 +7,7 @@ interface BackupConfigurationsProps {}
 
 interface Data {
   id: number;
-  calories: number;
-  carbs: number;
-  fat: number;
-  name: string;
-  protein: number;
+  [key: string]: any;
 }
 
 function createData(
