75db941d81cae0d758835aab19dcea1385062f94
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-21 19:03:44 +0200

    feat: Add TabSection component to StorageServerCreate for improved UI navigation

--- frontend/src/components/Dashboard/StorageServers/StorageServerCreate/StorageServerCreate.tsx
+++ frontend/src/components/Dashboard/StorageServers/StorageServerCreate/StorageServerCreate.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import TabSection from "@/components/TabSection";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Button, IconButton, Tooltip } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+
 interface StorageServerCreateProps {
   render: boolean;
   setRender: React.Dispatch<React.SetStateAction<boolean>>;
@@ -18,10 +23,58 @@ function StorageServerCreate({ render, setRender }: StorageServerCreateProps) {
   };
 
   return (
-    <>
-      <button onClick={handleGoBack}>Back</button>
-      <p>Create</p>
-    </>
+    <div style={{ position: "relative" }}>
+      <div
+        style={{
+          position: "absolute",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          top: "0",
+          left: "0",
+          zIndex: 1,
+          width: 80,
+          height: 80,
+        }}
+      >
+        <Tooltip title="Back" placement="right-start">
+          <IconButton aria-label="back" onClick={handleGoBack}>
+            <ArrowBackIcon fontSize="inherit" />
+          </IconButton>
+        </Tooltip>
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          bottom: "40px",
+          right: "40px",
+          zIndex: 1,
+        }}
+      >
+        <Button variant="contained" endIcon={<AddIcon />}>
+          Create
+        </Button>
+      </div>
+      <TabSection
+        tabs={[
+          {
+            label: "Basic",
+            component: <div>Create 1</div>,
+          },
+          {
+            label: "Connection",
+            component: <div>Create 2</div>,
+          },
+          {
+            label: "Driver",
+            component: <div>Create 3</div>,
+          },
+        ]}
+      />
+    </div>
   );
 }
 
