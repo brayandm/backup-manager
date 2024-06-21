37ee40224c904a41d4c178c15fb669a0d79d5989
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-21 18:33:30 +0200

    feat: Add TabSection component to frontend/src/components/TabSection

---
+++ frontend/src/components/TabSection/TabSection.tsx
@@ -0,0 +1,98 @@
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`,
+  };
+}
+
+interface TabSectionProps {
+  tabs: {
+    label: string;
+    component: React.ReactNode;
+  }[];
+}
+
+export default function TabSection({ tabs }: TabSectionProps) {
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index: number) => {
+    setValue(index);
+  };
+
+  return (
+    <Box sx={{ width: "86vw" }}>
+      <Box>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="secondary"
+          textColor="inherit"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+          sx={{
+            backgroundColor: theme.palette.secondary.dark,
+            "& 	.MuiTabs-indicator": {
+              backgroundColor: theme.palette.secondary.light,
+              top: "5vh",
+              height: "0.5vh",
+            },
+            "& .MuiTab-root": {
+              fontSize: "16px",
+            },
+          }}
+        >
+          {tabs.map((tab, index) => (
+            <Tab
+              key={index}
+              label={tab.label}
+              sx={{
+                color: theme.palette.common.white,
+              }}
+              {...a11yProps(index)}
+            />
+          ))}
+        </Tabs>
+      </Box>
+      {tabs.map((tab, index) => (
+        <TabPanel key={index} value={value} index={index} dir={theme.direction}>
+          {tab.component}
+        </TabPanel>
+      ))}
+    </Box>
+  );
+}
