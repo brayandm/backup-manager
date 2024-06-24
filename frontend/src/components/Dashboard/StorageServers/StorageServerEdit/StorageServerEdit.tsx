b47bed0165f0da26b5bd20b15a4c0b11dfd5ac5f
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-24 19:15:00 +0200

    chore: Refactor StorageServerEdit component to improve code structure and readability

--- frontend/src/components/Dashboard/StorageServers/StorageServerEdit/StorageServerEdit.tsx
+++ frontend/src/components/Dashboard/StorageServers/StorageServerEdit/StorageServerEdit.tsx
@@ -1,5 +1,15 @@
 "use client";
 
+import TabSection from "@/components/TabSection";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Alert, Button, IconButton, Tooltip } from "@mui/material";
+import { useState } from "react";
+import ConnectionForm from "@/components/ConnectionForm";
+import StorageServerDriverForm from "@/components/StorageServerDriverForm";
+import StorageServerBasicForm from "@/components/StorageServerBasicForm";
+import { post } from "@/lib/backendApi";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+
 interface StorageServerEditProps {
   id: string;
   render: boolean;
@@ -10,7 +20,6 @@ function StorageServerEdit({ id, render, setRender }: StorageServerEditProps) {
   const handleGoBack = () => {
     const searchParams = new URLSearchParams(window.location.search);
     searchParams.delete("option");
-    searchParams.delete("id");
     window.history.replaceState(
       {},
       "",
@@ -19,11 +28,129 @@ function StorageServerEdit({ id, render, setRender }: StorageServerEditProps) {
     setRender(!render);
   };
 
+  const [name, setName] = useState("");
+  const [connection, setConnection] = useState("[]");
+  const [driver, setDriver] = useState("{}");
+
+  const [basicTabMissingValues, setBasicTabMissingValues] = useState(true);
+  const [connectionTabMissingValues, setConnectionTabMissingValues] =
+    useState(true);
+  const [driverTabMissingValues, setDriverTabMissingValues] = useState(true);
+
+  const missingValues =
+    basicTabMissingValues ||
+    connectionTabMissingValues ||
+    driverTabMissingValues;
+  const [onError, setOnError] = useState(false);
+
   return (
-    <>
-      <button onClick={handleGoBack}>Back</button>
-      <p>Edit {id}</p>
-    </>
+    <div style={{ position: "relative" }}>
+      {onError && (
+        <div
+          style={{
+            position: "absolute",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "200px",
+            top: "-4vh",
+            left: "calc(50% - 100px)",
+            zIndex: 1,
+          }}
+        >
+          <Alert severity="error"> Missing values </Alert>
+        </div>
+      )}
+      <div
+        style={{
+          position: "absolute",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          top: "0",
+          left: "0",
+          zIndex: 1,
+          width: 80,
+          height: 80,
+        }}
+      >
+        <Tooltip title="Back" placement="right-start">
+          <IconButton aria-label="back" onClick={handleGoBack}>
+            <ArrowBackIcon fontSize="inherit" />
+          </IconButton>
+        </Tooltip>
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          bottom: "40px",
+          right: "40px",
+          zIndex: 1,
+        }}
+      >
+        <Button
+          variant="contained"
+          endIcon={<EditNoteIcon />}
+          onClick={async () => {
+            if (missingValues) {
+              setOnError(true);
+              setTimeout(() => {
+                setOnError(false);
+              }, 2000);
+            } else {
+              const res = await post("/storage-servers/store", {
+                name: name,
+                connection_config: connection,
+                driver_config: driver,
+              });
+            }
+          }}
+          disabled={missingValues}
+        >
+          Update
+        </Button>
+      </div>
+      <TabSection
+        tabs={[
+          {
+            missingValues: basicTabMissingValues,
+            label: "Basic",
+            component: (
+              <StorageServerBasicForm
+                name={name}
+                setName={setName}
+                setMissingValues={setBasicTabMissingValues}
+              />
+            ),
+          },
+          {
+            missingValues: connectionTabMissingValues,
+            label: "Connection",
+            component: (
+              <ConnectionForm
+                connection={connection}
+                setConnection={setConnection}
+                setMissingValues={setConnectionTabMissingValues}
+              />
+            ),
+          },
+          {
+            missingValues: driverTabMissingValues,
+            label: "Driver",
+            component: (
+              <StorageServerDriverForm
+                driver={driver}
+                setDriver={setDriver}
+                setMissingValues={setDriverTabMissingValues}
+              />
+            ),
+          },
+        ]}
+      />
+    </div>
   );
 }
 
