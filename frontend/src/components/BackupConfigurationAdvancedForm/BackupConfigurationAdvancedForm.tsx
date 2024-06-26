572b5f8de796dadc8906c3b395756fa21a589c89
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-27 01:12:53 +0200

    Refactor BackupConfigurationAdvancedForm to include retention policy, compression, encryption, and integrity check options

--- frontend/src/components/BackupConfigurationAdvancedForm/BackupConfigurationAdvancedForm.tsx
+++ frontend/src/components/BackupConfigurationAdvancedForm/BackupConfigurationAdvancedForm.tsx
@@ -3,6 +3,14 @@
 import React, { useEffect } from "react";
 
 interface BackupConfigurationAdvancedFormProps {
+  retentionPolicy: string;
+  setRetentionPolicy: React.Dispatch<React.SetStateAction<string>>;
+  compression: string;
+  setCompression: React.Dispatch<React.SetStateAction<string>>;
+  encryption: string;
+  setEncryption: React.Dispatch<React.SetStateAction<string>>;
+  integrityCheck: string;
+  setIntegrityCheck: React.Dispatch<React.SetStateAction<string>>;
   setMissingValues: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
