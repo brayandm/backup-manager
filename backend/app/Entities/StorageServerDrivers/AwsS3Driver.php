17bb0f032f727d438a5257806986cc8539e19b63
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-07-25 02:34:04 +0200

    refactor: Update AWS S3 driver push, pull, delete, setup, clean, and getFreeSpace methods
    The AWS S3 driver's push, pull, delete, setup, clean, and getFreeSpace methods are updated to include the necessary commands for interacting with the S3 bucket. This change improves the functionality and reliability of the AWS S3 driver by ensuring that the correct commands are executed for each operation.

--- backend/app/Entities/StorageServerDrivers/AwsS3Driver.php
+++ backend/app/Entities/StorageServerDrivers/AwsS3Driver.php
@@ -30,26 +30,48 @@ class AwsS3Driver implements StorageServerDriverInterface
 
     public function push(string $localWorkDir, string $backupName)
     {
+        $command = "aws s3 cp $localWorkDir s3://$this->bucket/$this->path/$backupName --recursive";
+
+        $command .= ' && rm -r -f '.$localWorkDir;
+
+        return $command;
     }
 
     public function pull(string $localWorkDir, string $backupName)
     {
+        $command = "mkdir $localWorkDir -p";
+
+        $command .= " && aws s3 cp s3://$this->bucket/$this->path/$backupName $localWorkDir --recursive";
+
+        return $command;
     }
 
     public function delete(string $backupName)
     {
+        $command = "aws s3 rm s3://$this->bucket/$this->path/$backupName --recursive";
+
+        return $command;
     }
 
     public function setup()
     {
+        $command = "true";
+
+        return $command;
     }
 
     public function clean()
     {
+        $command = "true";
+
+        return $command;
     }
 
     public function getFreeSpace()
     {
+        $command = "true";
+
+        return $command;
     }
 
     public function dockerContext(bool $dockerContext)
