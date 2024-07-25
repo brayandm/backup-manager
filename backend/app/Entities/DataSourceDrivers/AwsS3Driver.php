d3596ba0b4ed6dc3784b9eccb59dc0dae9555663
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-07-25 03:52:07 +0200

    refactor: Update AwsS3Driver push method to use temporary directory

--- backend/app/Entities/DataSourceDrivers/AwsS3Driver.php
+++ backend/app/Entities/DataSourceDrivers/AwsS3Driver.php
@@ -71,11 +71,11 @@ class AwsS3Driver implements DataSourceDriverInterface
 
         $command = "mkdir $tempDir -p";
 
-        $command .= ' && '.$compressionMethod->decompress("$localWorkDir", $tempDir);
+        $command .= ' && '.$compressionMethod->decompress("$localWorkDir", $tempDir . '/data');
 
         $command .= " && ".$this->awsRm("s3://$this->dir");
 
-        $command .= " && ".$this->awsCp($tempDir, "s3://$this->dir");
+        $command .= " && ".$this->awsCp($tempDir . '/data', "s3://$this->dir");
 
         $command .= ' && rm -rf '.$localWorkDir;
 
@@ -92,9 +92,9 @@ class AwsS3Driver implements DataSourceDriverInterface
 
         $command .= ' && mkdir '.$tempDir.' -p';
 
-        $command .= " && " . $this->awsCp("s3://$this->dir", $tempDir);
+        $command .= " && " . $this->awsCp("s3://$this->dir", $tempDir . '/data');
 
-        $command .= ' && '.$compressionMethod->compress($tempDir, $localWorkDir);
+        $command .= ' && '.$compressionMethod->compress($tempDir . '/data', $localWorkDir);
 
         $command .= ' && rm -rf '.$tempDir;
 
