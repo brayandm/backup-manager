828590f756e369d1b7b0d26d85438e4c9dd8d2d4
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-01 16:51:04 +0200

    feat: Add MigrationConfigurationFactory for generating migration configurations

---
+++ backend/database/factories/MigrationConfigurationFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Entities\CompressionMethodConfig;
+use App\Entities\Methods\CompressionMethods\TarCompressionMethod;
+use App\Models\DataSource;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BackupConfiguration>
+ */
+class MigrationConfigurationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->text(30),
+            'data_source_id' => DataSource::factory(),
+            'schedule_cron' => '0 0 * * *',
+            'compression_config' => new CompressionMethodConfig(
+                new TarCompressionMethod()
+            ),
+        ];
+    }
+}
