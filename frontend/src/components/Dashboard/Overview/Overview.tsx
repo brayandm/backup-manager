d40def92bf8e261b69aed195268bd569fb655eef
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-04 19:29:45 +0200

    Refactor Overview component to include week and month backup data

--- frontend/src/components/Dashboard/Overview/Overview.tsx
+++ frontend/src/components/Dashboard/Overview/Overview.tsx
@@ -3,12 +3,15 @@
 import React from "react";
 import { LineChart } from "@mui/x-charts/LineChart";
 import { format, subDays, subMonths } from "date-fns";
+import { Typography } from "@mui/material";
 
 interface OverviewProps {}
 
 function Overview({}: OverviewProps) {
   const weekBackupData = [5, 3, 6, 2, 8, 4, 7];
+  const weekMigrationData = [2, 1, 3, 1, 4, 2, 3];
   const monthBackupData = [10, 6, 12, 4, 16, 8, 14, 20, 18, 22, 24, 26];
+  const monthMigrationData = [4, 2, 6, 2, 8, 4, 6, 10, 9, 11, 12, 13];
 
   const today = new Date();
   const xLabelsWeek = Array.from({ length: 7 }, (_, i) =>
@@ -34,19 +37,41 @@ function Overview({}: OverviewProps) {
   );
 
   return (
-    <div>
-      <LineChart
-        width={500}
-        height={300}
-        series={[{ data: weekBackupData, label: "Backups last week" }]}
-        xAxis={[{ scaleType: "point", data: xLabelsAbbreviatedWeek }]}
-      />
-      <LineChart
-        width={500}
-        height={300}
-        series={[{ data: monthBackupData, label: "Backups last 12 months" }]}
-        xAxis={[{ scaleType: "point", data: xLabelsMonth }]}
-      />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: "20px",
+      }}
+    >
+      <div>
+        <Typography variant="h6" gutterBottom>
+          Backups & Migrations in the Last Week
+        </Typography>
+        <LineChart
+          width={500}
+          height={300}
+          series={[
+            { data: weekBackupData, label: "Backups" },
+            { data: weekMigrationData, label: "Migrations" },
+          ]}
+          xAxis={[{ scaleType: "point", data: xLabelsAbbreviatedWeek }]}
+        />
+      </div>
+      <div>
+        <Typography variant="h6" gutterBottom>
+          Backups & Migrations in the Last Month
+        </Typography>
+        <LineChart
+          width={500}
+          height={300}
+          series={[
+            { data: monthBackupData, label: "Backups" },
+            { data: monthMigrationData, label: "Migrations" },
+          ]}
+          xAxis={[{ scaleType: "point", data: xLabelsMonth }]}
+        />
+      </div>
     </div>
   );
 }
