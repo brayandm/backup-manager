420a0997752976b24b15ddeea79c264a2c5dbef3
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-04 19:23:53 +0200

    Refactor Overview component to include week and month backup data

--- frontend/src/components/Dashboard/Overview/Overview.tsx
+++ frontend/src/components/Dashboard/Overview/Overview.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { Typography } from "@mui/material";
 import React from "react";
 import { LineChart } from "@mui/x-charts/LineChart";
-import { format, subDays } from "date-fns";
+import { format, subDays, subMonths } from "date-fns";
 
 interface OverviewProps {}
 
@@ -12,8 +11,12 @@ function Overview({}: OverviewProps) {
   const monthBackupData = [10, 6, 12, 4, 16, 8, 14, 20, 18, 22, 24, 26];
 
   const today = new Date();
-  const xLabels = Array.from({ length: 7 }, (_, i) =>
+  const xLabelsWeek = Array.from({ length: 7 }, (_, i) =>
     format(subDays(today, i), "EEEE")
+  ).reverse();
+
+  const xLabelsMonth = Array.from({ length: 12 }, (_, i) =>
+    format(subMonths(today, i), "MMM")
   ).reverse();
 
   const dayAbbreviations: { [key: string]: string } = {
@@ -26,15 +29,23 @@ function Overview({}: OverviewProps) {
     Sunday: "Sun",
   };
 
-  const xLabelsAbbreviated = xLabels.map((day) => dayAbbreviations[day]);
+  const xLabelsAbbreviatedWeek = xLabelsWeek.map(
+    (day) => dayAbbreviations[day]
+  );
 
   return (
     <div>
       <LineChart
         width={500}
         height={300}
-        series={[{ data: weekBackupData, label: "Total backups in last week" }]}
-        xAxis={[{ scaleType: "point", data: xLabelsAbbreviated }]}
+        series={[{ data: weekBackupData, label: "Backups last week" }]}
+        xAxis={[{ scaleType: "point", data: xLabelsAbbreviatedWeek }]}
+      />
+      <LineChart
+        width={500}
+        height={300}
+        series={[{ data: monthBackupData, label: "Backups last 12 months" }]}
+        xAxis={[{ scaleType: "point", data: xLabelsMonth }]}
       />
     </div>
   );
