517e751064afda9897cdef85598ee73b7267ce95
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-06 19:10:05 +0200

    Refactor AnalyticsService to include storage server data in the overview

--- backend/app/Services/AnalyticsService.php
+++ backend/app/Services/AnalyticsService.php
@@ -18,7 +18,7 @@ class AnalyticsService
             'week_migration_data' => [1, 2],
             'month_backup_data' => [3, 4, 6],
             'month_migration_data' => [4, 3, 4],
-            'storage_servers' => StorageServer::all() -> map(function ($storageServer) {
+            'storage_servers' => StorageServer::all()->map(function ($storageServer) {
                 return [
                     'name' => $storageServer->name,
                     'used_space' => $storageServer->total_space_used,
