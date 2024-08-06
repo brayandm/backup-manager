ac381f4ae81817cf96cabb26c420ceadd486c698
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-06 17:20:30 +0200

    Refactor migrations table creation and deletion

--- backend/database/migrations/2024_08_06_150851_create_migrations_table.php
+++ backend/database/migrations/2024_08_06_150851_create_migrations_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('migration_configurations', function (Blueprint $table) {
+        Schema::create('migrations_table', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->foreignId('migration_configuration_id')->constrained();
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('migrations');
+        Schema::dropIfExists('migrations_table');
     }
 };
