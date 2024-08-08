d8fc9851b25fbd8b77575c25bb7937c3882ec5a0
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-08 17:22:23 +0200

    chore: Update Notifications component styling and add Telegram notifications functionality

--- frontend/src/components/Dashboard/Notifications/Notifications.tsx
+++ frontend/src/components/Dashboard/Notifications/Notifications.tsx
@@ -1,15 +1,67 @@
 "use client";
 
-import { Typography } from "@mui/material";
-import React from "react";
+import { Box, Checkbox, FormControlLabel, TextField } from "@mui/material";
+import React, { useState } from "react";
 
 interface NotificationsProps {}
 
 function Notifications({}: NotificationsProps) {
+  const [isTelegramNotificationsEnabled, setIsTelegramNotificationsEnabled] =
+    useState(false);
+  const [apiKey, setApiKey] = useState("");
+  const [channelId, setChannelId] = useState("");
+
   return (
-    <Typography variant="h2" sx={{ marginTop: "200px", textAlign: "center" }}>
-      Notifications Section
-    </Typography>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "84vw",
+        marginTop: "100px",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: "300px",
+          margin: "0 auto",
+        }}
+      >
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={isTelegramNotificationsEnabled}
+              onChange={(e) =>
+                setIsTelegramNotificationsEnabled(e.target.checked)
+              }
+              name="telegramNotifications"
+            />
+          }
+          label="Telegram Notifications Enabled"
+        />
+        <TextField
+          label="API Key"
+          variant="outlined"
+          value={apiKey}
+          required
+          disabled={!isTelegramNotificationsEnabled}
+          onChange={(e) => setApiKey(e.target.value)}
+          margin="normal"
+        />
+        <TextField
+          label="Channel ID"
+          variant="outlined"
+          value={channelId}
+          required
+          disabled={!isTelegramNotificationsEnabled}
+          onChange={(e) => setChannelId(e.target.value)}
+          margin="normal"
+        />
+      </Box>
+    </div>
   );
 }
 
