77a631e0c57006af97bd8a2e93eb3a525ce3ac0c
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-08 16:57:22 +0200

    Refactor formatBytes method in Formatting class for consistent code style

--- backend/app/Helpers/Formatting.php
+++ backend/app/Helpers/Formatting.php
@@ -4,15 +4,16 @@ namespace App\Helpers;
 
 class Formatting
 {
-    public static function formatBytes($bytes) {
+    public static function formatBytes($bytes)
+    {
         if ($bytes === 0) {
-            return "0 Bytes";
+            return '0 Bytes';
         }
 
         $k = 1024;
-        $sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+        $sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
         $i = floor(log($bytes) / log($k));
 
-        return round($bytes / pow($k, $i), 2) . " " . $sizes[$i];
+        return round($bytes / pow($k, $i), 2).' '.$sizes[$i];
     }
 }
