2efac3a8784308e1fe1c3cc81cd2aa4689608096
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-12 19:08:52 +0200

    Refactor backup and migration scheduling logic

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -6,9 +6,12 @@ use App\Enums\BackupConfigurationStatus;
 use App\Enums\MigrationConfigurationStatus;
 use App\Jobs\BackupJob;
 use App\Jobs\CalculateFreeSpaceStorageServerJob;
+use App\Jobs\CheckDataSourceAvailabilityJob;
+use App\Jobs\CheckStorageServerAvailabilityJob;
 use App\Jobs\MigrationJob;
 use App\Jobs\RetentionPolicyJob;
 use App\Models\BackupConfiguration;
+use App\Models\DataSource;
 use App\Models\MigrationConfiguration;
 use App\Models\StorageServer;
 use Illuminate\Console\Scheduling\Schedule;
@@ -50,6 +53,14 @@ class Kernel extends ConsoleKernel
 
         foreach ($storageServers as $storageServer) {
             $schedule->job(new CalculateFreeSpaceStorageServerJob($storageServer))->everyFiveMinutes();
+
+            $schedule->job(new CheckStorageServerAvailabilityJob($storageServer))->everyFiveMinutes();
+        }
+
+        $dataSources = DataSource::all();
+
+        foreach ($dataSources as $dataSource) {
+            $schedule->job(new CheckDataSourceAvailabilityJob($dataSource))->everyFiveMinutes();
         }
     }
 
