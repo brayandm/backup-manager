196ef26c0df86077fbc110da835759f79cbb806c
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-14 20:53:16 +0200

    Refactor DynamicConfigServiceProvider to handle database connection errors gracefully

--- backend/app/Providers/DynamicConfigServiceProvider.php
+++ backend/app/Providers/DynamicConfigServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Settings;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,21 +23,26 @@ class DynamicConfigServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (Schema::hasTable('settings')) {
+        try {
+            DB::connection()->getPdo();
 
-            Settings::firstOrCreate(['key' => 'telegram_bot_active'], ['value' => 'false']);
-            Settings::firstOrCreate(['key' => 'telegram_bot_api_key'], ['value' => '']);
-            Settings::firstOrCreate(['key' => 'telegram_channel_id'], ['value' => '']);
-            Settings::firstOrCreate(['key' => 'telegram_notify_backups'], ['value' => 'false']);
-            Settings::firstOrCreate(['key' => 'telegram_notify_migrations'], ['value' => 'false']);
+            if (Schema::hasTable('settings')) {
 
-            $settings = Settings::all()->pluck('value', 'key')->toArray();
+                Settings::firstOrCreate(['key' => 'telegram_bot_active'], ['value' => 'false']);
+                Settings::firstOrCreate(['key' => 'telegram_bot_api_key'], ['value' => '']);
+                Settings::firstOrCreate(['key' => 'telegram_channel_id'], ['value' => '']);
+                Settings::firstOrCreate(['key' => 'telegram_notify_backups'], ['value' => 'false']);
+                Settings::firstOrCreate(['key' => 'telegram_notify_migrations'], ['value' => 'false']);
 
-            Config::set('logging.channels.telegram.active', $settings['telegram_bot_active']);
-            Config::set('logging.channels.telegram.handler_with.apiKey', $settings['telegram_bot_api_key']);
-            Config::set('logging.channels.telegram.handler_with.channel', $settings['telegram_channel_id']);
-            Config::set('logging.channels.telegram.notify_backups', $settings['telegram_notify_backups']);
-            Config::set('logging.channels.telegram.notify_migrations', $settings['telegram_notify_migrations']);
+                $settings = Settings::all()->pluck('value', 'key')->toArray();
+
+                Config::set('logging.channels.telegram.active', $settings['telegram_bot_active']);
+                Config::set('logging.channels.telegram.handler_with.apiKey', $settings['telegram_bot_api_key']);
+                Config::set('logging.channels.telegram.handler_with.channel', $settings['telegram_channel_id']);
+                Config::set('logging.channels.telegram.notify_backups', $settings['telegram_notify_backups']);
+                Config::set('logging.channels.telegram.notify_migrations', $settings['telegram_notify_migrations']);
+            }
+        } catch (\Exception $e) {
         }
     }
 }
