b14f5bedb7e8a1d085392ba0b65476d3d7f613a1
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 17:46:32 +0200

    Create release 1.0.0

---
+++ uninstall.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+KEEP_VOLUMES=false
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --keep-volumes) KEEP_VOLUMES=true ;;
+        *) echo "Unknown option: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+if [ -d "/opt/backup-manager" ]; then
+  echo "Uninstalling Backup Manager from /opt/backup-manager."
+  cd /opt/backup-manager
+  VERSION=$(cat VERSION) 2>/dev/null || true
+
+  if [ "$KEEP_VOLUMES" = true ]; then
+    echo "Keeping volumes."
+    VERSION=$VERSION docker compose down --remove-orphans --rmi all 2>/dev/null || true
+  else
+    VERSION=$VERSION docker compose down --volumes --remove-orphans --rmi all 2>/dev/null || true
+  fi
+
+  rm -rf /opt/backup-manager
+  echo "Backup Manager has been uninstalled."
+else
+  echo "Backup Manager is not installed."
+fi
