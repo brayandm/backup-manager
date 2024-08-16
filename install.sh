7d7d7f1fb598c3eb53778b101bf5e0f6392c628c
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 12:30:16 +0200

    chore: Update install.sh to fetch docker-compose.yml dynamically based on version

--- install.sh
+++ install.sh
@@ -7,10 +7,5 @@ source .env
 mkdir -p /opt/backup-manager
 cd /opt/backup-manager
 
-if [ -f docker-compose.yml ]; then
-    VERSION=$VERSION docker compose down --volumes --remove-orphans --rmi all
-fi
-
-
 curl -o docker-compose.yml https://raw.githubusercontent.com/brayandm/backup-manager/$VERSION/docker-compose.yml
 VERSION=$VERSION docker compose up -d
