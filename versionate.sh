4c2271bfc7c3d1c18261ced809511f7799e5143c
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 21:08:30 +0200

    Create release 1.0.0

--- versionate.sh
+++ versionate.sh
@@ -4,16 +4,22 @@ bash genreadme.sh
 
 version=$(cat VERSION)
 
+if ! grep -Fxq "$version" versions.txt; then
+    echo "$version" >> versions.txt
+    echo "Version $version added to versions.txt"
+else
+    echo "Version $version already exists in versions.txt"
+fi
+
 git add .
 
 git commit -m "Create release $version" || true
 
 git push origin main
 
-git tag -d $version || true
+git tag -d "$version" || true
+git push origin --delete "$version" || true
 
-git push origin --delete $version || true
-
-git tag -a $version -m "Release $version"
+git tag -a "$version" -m "Release $version"
 
 git push origin "$version"
