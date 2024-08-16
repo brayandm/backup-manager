36992ac18d4f3b2170287f21f5779118f831a225
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 11:53:57 +0200

    chore: Update Docker images to use specific version tags
    ```

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VERSION=1.0.0
+
 cd backend/
 docker compose -f docker-compose.build.yml build --compress --force-rm --no-cache --parallel --pull
 docker compose -f docker-compose.build.yml push
--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VERSION=1.0.0
+
 curl -o docker-compose.yml https://raw.githubusercontent.com/brayandm/backup-manager/main/docker-compose.yml
 docker compose up -d
 open http://localhost:3001
