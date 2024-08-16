adea8ca1b9a7aea8ee71f1d25bd91e971281bb1b
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 12:23:58 +0200

    chore: Update build and installation scripts to use environment variable for version number

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ source .env
 
 cd backend/
 VERSION=$VERSION docker compose -f docker-compose.build.yml build --compress --force-rm --no-cache --parallel --pull
-docker compose -f docker-compose.build.yml push
+VERSION=$VERSION docker compose -f docker-compose.build.yml push
 
 cd ../frontend/
 VERSION=$VERSION docker compose -f docker-compose.build.yml build --compress --force-rm --no-cache --parallel --pull 
-docker compose -f docker-compose.build.yml push
+VERSION=$VERSION docker compose -f docker-compose.build.yml push
