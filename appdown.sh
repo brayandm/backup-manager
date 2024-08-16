cfa9fa47a2f0189b9d2a02be34cb242e63c8ab90
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 17:20:22 +0200

    chore: Update up.sh script to stop Backup Manager
    The up.sh script has been modified to include a check for Docker Compose running status and stop the Backup Manager if it is currently running. This change improves the functionality of the script by ensuring that the Backup Manager is properly stopped before any further actions are taken.

--- appdown.sh
+++ appdown.sh
@@ -5,42 +5,15 @@ set -e
 if [ -d "/opt/backup-manager" ]; then
 
   echo "Checking if Docker Compose is running..."
-  if docker compose ps | grep -q "Up"; then
-    echo "Backup Manager is running."
-    exit 0
-  else
+  if ! docker compose ps | grep -q "Up"; then
     echo "Backup Manager is not running."
+    exit 0
   fi
 
-  DEFAULT_PORT=49160
-
-  is_port_in_use() {
-    if ss -tuln | grep -q ":$1 "; then
-      return 0
-    else
-      return 1
-    fi
-  }
-
-  while true; do
-    read -p "Please enter the desired port for the application (1024-65535) [default: $DEFAULT_PORT]: " PORT
-
-    PORT=${PORT:-$DEFAULT_PORT}
-
-    if ! [[ "$PORT" =~ ^[0-9]+$ ]] || [ "$PORT" -lt 1024 ] || [ "$PORT" -gt 65535 ]; then
-      echo "Please enter a valid number between 1024 and 65535."
-    elif is_port_in_use $PORT; then
-      echo "Port $PORT is already in use. Please choose another port."
-    else
-      echo "Port $PORT is available."
-      break
-    fi
-  done
-
-  echo "Starting Backup Manager."
+  echo "Stopping Backup Manager."
   VERSION=$(cat VERSION)
-  APP_PORT=$PORT VERSION=$VERSION docker compose up -d
-  echo "Backup Manager is running in http://localhost:$PORT."
+  VERSION=$VERSION docker compose down --remove-orphans
+  echo "Backup Manager has been stopped."
 else
   echo "Backup Manager is not installed."
 fi
