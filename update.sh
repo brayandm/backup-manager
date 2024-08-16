e7b4c7fdf69d311febd7a092d725455f62ad41d4
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 12:50:18 +0200

    Update update.sh to use environment variable for version number

--- update.sh
+++ update.sh
@@ -2,12 +2,11 @@
 
 set -e
 
-XVERSION=1.0.0
-
 if [ -d "/opt/backup-manager" ]; then
-  echo "Uninstalling Backup Manager from /opt/backup-manager but keeping the data."
+  echo "Uninstalling Backup Manager from /opt/backup-manager."
   cd /opt/backup-manager
-  VERSION=$XVERSION docker compose down --remove-orphans --rmi all
+  VERSION=cat VERSION
+  VERSION=$VERSION docker compose down --volumes --remove-orphans --rmi all
   rm -rf /opt/backup-manager
   echo "Backup Manager has been uninstalled."
 else
@@ -20,7 +19,8 @@ else
   echo "Installing Backup Manager in /opt/backup-manager."
   mkdir -p /opt/backup-manager
   cd /opt/backup-manager
-  curl -o docker-compose.yml https://raw.githubusercontent.com/brayandm/backup-manager/$XVERSION/docker-compose.yml
-  VERSION=$XVERSION docker compose up -d
+  echo $VERSION > VERSION
+  curl -o docker-compose.yml https://raw.githubusercontent.com/brayandm/backup-manager/$VERSION/docker-compose.yml
+  VERSION=$VERSION docker compose up -d
   echo "Backup Manager has been installed."
 fi
