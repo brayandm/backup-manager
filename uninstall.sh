d06bacd48422aced77a68d4ddff5fb9baf583218
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 13:06:54 +0200

    chore: Fix uninstall script to correctly retrieve version number from file

--- uninstall.sh
+++ uninstall.sh
@@ -5,7 +5,7 @@ set -e
 if [ -d "/opt/backup-manager" ]; then
   echo "Uninstalling Backup Manager from /opt/backup-manager."
   cd /opt/backup-manager
-  VERSION=cat VERSION
+  VERSION=$(cat VERSION)
   VERSION=$VERSION docker compose down --volumes --remove-orphans --rmi all
   rm -rf /opt/backup-manager
   echo "Backup Manager has been uninstalled."
