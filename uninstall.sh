7e347e3de3e0b28f0ec6f926ca1c8d7ceb175fa6
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 12:49:44 +0200

    Update uninstall.sh to dynamically fetch the version from VERSION file

--- uninstall.sh
+++ uninstall.sh
@@ -2,12 +2,11 @@
 
 set -e
 
-XVERSION=1.0.0
-
 if [ -d "/opt/backup-manager" ]; then
   echo "Uninstalling Backup Manager from /opt/backup-manager."
   cd /opt/backup-manager
-  VERSION=$XVERSION docker compose down --volumes --remove-orphans --rmi all
+  VERSION=cat VERSION
+  VERSION=$VERSION docker compose down --volumes --remove-orphans --rmi all
   rm -rf /opt/backup-manager
   echo "Backup Manager has been uninstalled."
 else
