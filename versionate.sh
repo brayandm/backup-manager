8fe70015a893c9d341fdb7fe14a6c8d9ac38b124
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 19:12:32 +0200

    Create release 1.0.0

--- versionate.sh
+++ versionate.sh
@@ -8,12 +8,12 @@ git add .
 
 git commit -m "Create release $version" || true
 
+git push origin main
+
 git tag -d $version || true
 
 git push origin --delete $version || true
 
 git tag -a $version -m "Release $version"
 
-git push origin main
-
-# git push origin "$version"
+git push origin "$version"
