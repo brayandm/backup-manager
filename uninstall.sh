ebcd2d55a6df12c60c02828618a6486ce125f476
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 15:59:59 +0200

    chore: Update uninstall script to handle volume cleanup

--- uninstall.sh
+++ uninstall.sh
@@ -2,11 +2,28 @@
 
 set -e
 
+KEEP_VOLUMES=false
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --keep-volumes) KEEP_VOLUMES=true ;;
+        *) echo "Unknown option: $1"; exit 1 ;;
+    esac
+    shift
+done
+
 if [ -d "/opt/backup-manager" ]; then
   echo "Uninstalling Backup Manager from /opt/backup-manager."
   cd /opt/backup-manager
   VERSION=$(cat VERSION)
-  VERSION=$VERSION docker compose down --volumes --remove-orphans --rmi all
+
+  if [ "$KEEP_VOLUMES" = true ]; then
+    echo "Keeping volumes."
+    VERSION=$VERSION docker compose down --remove-orphans --rmi all
+  else
+    VERSION=$VERSION docker compose down --volumes --remove-orphans --rmi all
+  fi
+
   rm -rf /opt/backup-manager
   echo "Backup Manager has been uninstalled."
 else
