9c7a477c2b16d9d2bfb8a506c728ed7ff859e888
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 12:46:36 +0200

    chore: Update build and installation scripts to use environment variable for version number

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-XVERSION=1.0.0
+source VERSION
 
 cd backend/
-VERSION=$XVERSION docker compose -f docker-compose.build.yml build --compress --force-rm --no-cache --parallel --pull
-VERSION=$XVERSION docker compose -f docker-compose.build.yml push
+VERSION=$VERSION docker compose -f docker-compose.build.yml build --compress --force-rm --no-cache --parallel --pull
+VERSION=$VERSION docker compose -f docker-compose.build.yml push
 XVERSION
 cd ../frontend/
-VERSION=$XVERSION docker compose -f docker-compose.build.yml build --compress --force-rm --no-cache --parallel --pull 
-VERSION=$XVERSION docker compose -f docker-compose.build.yml push
+VERSION=$VERSION docker compose -f docker-compose.build.yml build --compress --force-rm --no-cache --parallel --pull 
+VERSION=$VERSION docker compose -f docker-compose.build.yml push
