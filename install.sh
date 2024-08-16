21b557cb7052007df3f8dcb3ab9c641c9d6facad
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 12:15:42 +0200

    chore: Update installation script to create backup-manager directory and execute start.sh with sudo

--- install.sh
+++ install.sh
@@ -7,5 +7,7 @@ VERSION=1.0.0
 mkdir -p /opt/backup-manager
 cd /opt/backup-manager
 
+VERSION=$VERSION docker compose down --volumes --remove-orphans --rmi all
+
 curl -o docker-compose.yml https://raw.githubusercontent.com/brayandm/backup-manager/main/docker-compose.yml
 VERSION=$VERSION docker compose up -d
