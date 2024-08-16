286635acef2879b839c4089b513819ee0f4ad8d4
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 11:55:30 +0200

    chore: Update build script to include version argument in docker compose commands

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 VERSION=1.0.0
+export VERSION
 
 cd backend/
-docker compose -f docker-compose.build.yml build --compress --force-rm --no-cache --parallel --pull
+docker compose -f docker-compose.build.yml build --compress --force-rm --no-cache --parallel --pull --build-arg VERSION=$VERSION
 docker compose -f docker-compose.build.yml push
 
 cd ../frontend/
-docker compose -f docker-compose.build.yml build --compress --force-rm --no-cache --parallel --pull
+docker compose -f docker-compose.build.yml build --compress --force-rm --no-cache --parallel --pull --build-arg VERSION=$VERSION
 docker compose -f docker-compose.build.yml push
