005fdc024478611717941b7175f11ef4481cb866
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 16:06:42 +0200

    chore: Update update.sh script to use XVERSION for installation

--- update.sh
+++ update.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+cleanup() {
+    rm -f uninstall.sh install.sh
+}
+
+trap cleanup EXIT
+
 curl -o uninstall.sh https://raw.githubusercontent.com/brayandm/backup-manager/XVERSION/uninstall.sh
 curl -o install.sh https://raw.githubusercontent.com/brayandm/backup-manager/XVERSION/install.sh
 
@@ -11,6 +17,3 @@ chmod +x install.sh
 sudo ./uninstall.sh --keep-volumes
 
 sudo VERSION=XVERSION ./install.sh
-
-rm uninstall.sh
-rm install.sh
