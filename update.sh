132c8ed2741f70312de0094b8e5dfeaa61e09db9
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 12:50:40 +0200

    Update update.sh to use environment variable for version number

--- update.sh
+++ update.sh
@@ -5,8 +5,8 @@ set -e
 if [ -d "/opt/backup-manager" ]; then
   echo "Uninstalling Backup Manager from /opt/backup-manager."
   cd /opt/backup-manager
-  VERSION=cat VERSION
-  VERSION=$VERSION docker compose down --volumes --remove-orphans --rmi all
+  OLDVERSION=cat VERSION
+  VERSION=$OLDVERSION docker compose down --volumes --remove-orphans --rmi all
   rm -rf /opt/backup-manager
   echo "Backup Manager has been uninstalled."
 else
