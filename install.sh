d5ac4159edc505349fa05145252f26d8e9b71f0a
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 12:32:22 +0200

    chore: Update install.sh to conditionally execute docker compose commands based on the presence of docker-compose.yml

--- install.sh
+++ install.sh
@@ -4,8 +4,12 @@ set -e
 
 source .env
 
-mkdir -p /opt/backup-manager
-cd /opt/backup-manager
-
-curl -o docker-compose.yml https://raw.githubusercontent.com/brayandm/backup-manager/$VERSION/docker-compose.yml
-VERSION=$VERSION docker compose up -d
+if [ -d "/opt/backup-manager" ]; then
+  echo "Backup Manager is already installed."
+else
+  echo "Installing Backup Manager in /opt/backup-manager."
+  mkdir -p /opt/backup-manager
+  cd /opt/backup-manager
+  curl -o docker-compose.yml https://raw.githubusercontent.com/brayandm/backup-manager/$VERSION/docker-compose.yml
+  VERSION=$VERSION docker compose up -d
+fi
