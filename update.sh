b9b63536fff54deb0174d2de93ecf8da388eb6bb
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-16 16:05:55 +0200

    chore: Update uninstall and install scripts

--- update.sh
+++ update.sh
@@ -2,25 +2,15 @@
 
 set -e
 
-if [ -d "/opt/backup-manager" ]; then
-  echo "Uninstalling Backup Manager from /opt/backup-manager but keeping the data."
-  cd /opt/backup-manager
-  OLDVERSION=$(cat VERSION)
-  VERSION=$OLDVERSION docker compose down --remove-orphans --rmi all
-  rm -rf /opt/backup-manager
-  echo "Backup Manager has been uninstalled."
-else
-  echo "Backup Manager is not installed."
-fi
+curl -o uninstall.sh https://raw.githubusercontent.com/brayandm/backup-manager/XVERSION/uninstall.sh
+curl -o install.sh https://raw.githubusercontent.com/brayandm/backup-manager/XVERSION/install.sh
 
-if [ -d "/opt/backup-manager" ]; then
-  echo "Backup Manager is already installed."
-else
-  echo "Installing Backup Manager in /opt/backup-manager."
-  mkdir -p /opt/backup-manager
-  cd /opt/backup-manager
-  echo $VERSION > VERSION
-  curl -o docker-compose.yml https://raw.githubusercontent.com/brayandm/backup-manager/$VERSION/docker-compose.yml
-  VERSION=$VERSION docker compose up -d
-  echo "Backup Manager has been installed."
-fi
+chmod +x uninstall.sh
+chmod +x install.sh
+
+sudo ./uninstall.sh --keep-volumes
+
+sudo VERSION=XVERSION ./install.sh
+
+rm uninstall.sh
+rm install.sh
