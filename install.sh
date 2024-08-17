4c5f5dca92cb58f6a7918f39452a9f4d4a14e64c
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-17 20:54:13 +0200

    Refactor backup-manager installation scripts

--- install.sh
+++ install.sh
@@ -15,7 +15,19 @@ else
   echo "Installing Backup Manager in ~/.local/backup-manager."
   mkdir -p ~/.local/backup-manager
   cd ~/.local/backup-manager
+
   echo $VERSION > VERSION
+
+  curl --silent -o uninstall https://raw.githubusercontent.com/brayandm/backup-manager/$version/uninstall.sh
+  curl --silent -o start https://raw.githubusercontent.com/brayandm/backup-manager/$version/start.sh
+  curl --silent -o stop https://raw.githubusercontent.com/brayandm/backup-manager/$version/stop.sh
+  curl --silent -o open https://raw.githubusercontent.com/brayandm/backup-manager/$version/open.sh
+  
+  chmod +x uninstall
+  chmod +x start
+  chmod +x stop
+  chmod +x open
+
   curl -o docker-compose.yml https://raw.githubusercontent.com/brayandm/backup-manager/$VERSION/docker-compose.yml
   VERSION=$VERSION docker compose pull
   echo "Backup Manager has been installed."
