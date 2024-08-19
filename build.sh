a4d023b23fcdc7468fa82fdb0ed275189edc73ab
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-08-19 15:10:23 +0200

    Create release 1.0.1

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ VERSION=$(cat VERSION)
 cd backend/
 VERSION=$VERSION docker compose -f docker-compose.build.yml build --compress --force-rm --no-cache --parallel --pull
 VERSION=$VERSION docker compose -f docker-compose.build.yml push
-XVERSION
+
 cd ../frontend/
 VERSION=$VERSION docker compose -f docker-compose.build.yml build --compress --force-rm --no-cache --parallel --pull 
 VERSION=$VERSION docker compose -f docker-compose.build.yml push
